b8b491b74f534e7aecd6e8861c1d17b1465476e2
pedroolvr19 <pg9967740@gmail.com>
2026-07-21 15:48:03 -0300

    getters e setters

--- src/main/java/dev/java10x/Fridge/model/Food.java
+++ src/main/java/dev/java10x/Fridge/model/Food.java
@@ -1,5 +1,62 @@
 package dev.java10x.Fridge.model;
 
-public class Food {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "food_table")
+public class Food {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Integer quantity;
+    private String expirationDate;
+
+    public Food() {
+    }
+
+    // construtor
+    public Food(Long id, String name, Integer quantity, String expirationDate) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.expirationDate = expirationDate;
+    }
+
+    // getters e setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(String expirationDate) {
+        this.expirationDate = expirationDate;
+    }
 }
---
+++ src/main/java/dev/java10x/Fridge/repository/Foodrepository.java
@@ -0,0 +1,5 @@
+package dev.java10x.Fridge.repository;
+
+public class FoodRepository {
+
+}
